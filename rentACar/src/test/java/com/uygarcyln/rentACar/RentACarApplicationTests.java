e9d943565a7b24a65a801db0f69dca942547a21e
Uygar Öztürk Ceylan <89963503+uodev@users.noreply.github.com>
2022-10-26 06:14:52 +0300

    Rent a Car Project

---
+++ rentACar/src/test/java/com/uygarcyln/rentACar/RentACarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uygarcyln.rentACar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentACarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
